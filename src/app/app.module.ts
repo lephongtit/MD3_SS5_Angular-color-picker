562b424c53bf0126cc190185b6b1835a4ca99570
Lê Hoàng Thiên Phong <thienphong251199@gmail.com>
2019-11-04 15:20:00 +0700

    oke

---
+++ src/app/app.module.ts
@@ -0,0 +1,20 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { ColorPickerComponent } from './color-picker/color-picker.component';
+import {ReactiveFormsModule} from "@angular/forms";
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ColorPickerComponent
+  ],
+  imports: [
+    BrowserModule,
+    ReactiveFormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
